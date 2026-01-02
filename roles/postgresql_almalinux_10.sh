d41b0814859792f50003c943e21141a650a464f1
skillmio <skillmiocfs@gmail.com>
2026-01-02 04:06:43 +0100

    Update postgresql_almalinux_10.sh

--- roles/postgresql_almalinux_10.sh
+++ roles/postgresql_almalinux_10.sh
@@ -60,7 +60,9 @@ SERVER_IP=${SERVER_IP:-$(hostname -I | awk '{print $1}')}
 
 info_msg "${MSG_USING_IP}: $SERVER_IP"
 
+echo""
 echo " --- "
+echo""
 
 # --- [1/5] INSTALLING PREREQUISITES ---
 info_msg "[1/5] ${MSG_INSTALL_PREREQUISITES}"
