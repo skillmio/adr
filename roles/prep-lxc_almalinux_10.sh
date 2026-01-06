1150490b17e6e767bdfda902175951d15b25c6ba
skillmio <skillmiocfs@gmail.com>
2026-01-06 05:15:37 +0100

    Create prep-lxc_almalinux_10.sh

---
+++ roles/prep-lxc_almalinux_10.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# ==========================================================================
+# ADR's Proxmox LXC installation script - AL10
+# ==========================================================================
+
+# === PREPARATIONS ===
+
+# --- LANGUAGE ---
+CONFIG_FILE="$HOME/.config/adr/config"
+LOCALES_DIR="$HOME/.config/adr/locales"
+
+if [[ -f "$CONFIG_FILE" ]]; then
+  source "$CONFIG_FILE"
+else
+  LANG_CODE="en"
+fi
+
+if [[ -f "$LOCALES_DIR/$LANG_CODE/messages.sh" ]]; then
+  source "$LOCALES_DIR/$LANG_CODE/messages.sh"
+else
+  info_msg "Locale $LANG_CODE not found, falling back to English."
+  source "$LOCALES_DIR/en/messages.sh"
+fi
+
+# === EXECUTION FLOW ===
+
+# --- Hello Msg ---
+info_msg "${MSG_START}"
+info_msg "${MSG_LOGPATH}"
+
+# --- [1/4] INSTALLING PREREQUISITES ---
+info_msg "[1/1] ${MSG_INSTALL_PREREQUISITES}"
+
+# Repo
+sudo /usr/bin/crb enable
+sudo dnf install -y epel-release
+
+# Packages
+sudo dnf install -y \
+  wget \
+  tcpdump \
+  bind-utils \
+  ncurses-term \
+  htop \
+  net-tools \
+  telnet \
+  traceroute \
+  iputils \
+  curl \
+  jq \
+  vim \
+  less \
+  NetworkManager NetworkManager-tui \
+  nano \
+  man \
+  strace \
+  lsof \
+  sysstat \
+  iotop \
+  atop \
+  iproute \
+  whois \
+  ethtool \
+  nmap \
+  ncurses \
+  mtr \
+  firewalld \
+  openssh-server \
+  nc
+
+# Update
+sudo dnf update -y && sudo dnf upgrade -y
+
